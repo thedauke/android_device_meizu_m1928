55db39104ae8d213bbf8c5b86000bf72bd2ce84f
TDADS <thedauke@gmail.com>
2020-10-10 14:36:36 +0000

    sm8150: merge overlays into one

--- common.mk
+++ common.mk
@@ -8,8 +8,7 @@ $(call inherit-product, vendor/meizu/sm8150-common/sm8150-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay 
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
